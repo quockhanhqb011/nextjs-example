5ef056fad770860e870c6b2737efd669b59bf2e5
khanhtran <khanh.tran@hoanghacgroup.com>
2022-07-06 12:07:56 +0700

    Update Nextjs

---
+++ ktran-app/pages/test/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import { Fragment } from 'react';
+
+const Index = () => {
+    return (
+        <Fragment>
+            <Head>
+                <title>Ktran Testing....</title>
+            </Head>
+
+            <div className={'body'}>
+                TEST TEST TEST
+            </div>
+        </Fragment>
+    );
+};
+
+export default Index
